832437a23591f3887c8236706e33f6aab80e14fb
Shivangi Chahar <chaharshivi15@gmail.com>
2025-11-20 08:47:23 +0530

    Delete js_assignment directory

--- js_assignment/Q10_CitizenEligibilityValidator.js
+++
@@ -1,9 +0,0 @@
-
-// Q10. Citizen Eligibility Validator
-const age = 19;
-const isCitizen = true;
-
-if (isCitizen && age >= 21) console.log("Eligible for all services.");
-else if (isCitizen && age >= 18) console.log("Eligible to vote only.");
-else if (!isCitizen && age >= 18) console.log("Only age criteria met.");
-else console.log("Not eligible yet.");
